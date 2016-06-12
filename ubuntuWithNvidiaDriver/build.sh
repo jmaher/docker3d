6b6a391beccc55c3722260b9ff501335eeeda12b
Joel Maher <joel.maher@gmail.com>
2016-06-12 02:53:42 -0700

    changes to docker test mozilla performance tests; adding libraries and tools

--- ubuntuWithNvidiaDriver/build.sh
+++ ubuntuWithNvidiaDriver/build.sh
@@ -1,7 +1,9 @@
 #!/bin/sh
+export PATH=$PATH:/usr/sbin
 
+wget http://us.download.nvidia.com/XFree86/Linux-x86_64/361.45.11/NVIDIA-Linux-x86_64-361.45.11.run
 IMAGE_NAME=ubuntu_with_nvidia_driver
-NVIDIA_DRIVER=NVIDIA-Linux-x86_64-337.12.run  # path to nvidia driver
+NVIDIA_DRIVER=NVIDIA-Linux-x86_64-361.45.11.run
 
 cp ${NVIDIA_DRIVER} NVIDIA-DRIVER.run
-sudo docker build -t ${IMAGE_NAME} .
+sudo docker build -t elvis314/${IMAGE_NAME}:latest .
